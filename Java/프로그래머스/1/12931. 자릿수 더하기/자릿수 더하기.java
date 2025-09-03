946c27f2446f5c474b041219ffee737c3039a33b
Seonoo <30854018+seonu5849@users.noreply.github.com>
2025-09-03 23:41:13 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

--- Java/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ Java/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,10 +4,10 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        String[] arrStr = String.valueOf(n).split("");
-        for(String str : arrStr) {
-            int num = Integer.parseInt(str);
-            answer += num;
+        while(n != 0) {
+            answer += n%10;
+            n /= 10;
+            
         }
 
         return answer;
