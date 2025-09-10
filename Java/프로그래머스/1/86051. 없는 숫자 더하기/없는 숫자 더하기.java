ba286d9a299731e61c443b123774df81cd5c9505
Seonoo <30854018+seonu5849@users.noreply.github.com>
2025-09-10 22:40:09 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

--- Java/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ Java/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -3,18 +3,10 @@ class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
         
-        List<Integer> list = new ArrayList<>();
         for(int number : numbers) {
-            list.add(number);
+            answer += number;
         }
         
-        // 없는 숫자들의 합계 구하기
-        for(int i=0; i<=9; i++) {
-            if(!list.contains(i)) {
-                answer += i;
-            }
-        }
-        
-        return answer;
+        return 45 - answer;
     }
 }
