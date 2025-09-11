6c36f6bfc8c9a5e7c72d50ca94757b4afd26e30f
Seonoo <30854018+seonu5849@users.noreply.github.com>
2025-09-11 22:46:56 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 27.94 ms, Memory: 76.5 MB -BaekjoonHub

--- Java/프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ Java/프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,43 +1,17 @@
+import java.util.*;
+import java.util.stream.*;
 class Solution {
     public int[] solution(int[] arr, int divisor) {
-        int[] answer = {};
+        int[] answer = Arrays.stream(arr)
+                            .filter(x -> x % divisor == 0) // 나누어 떨어지는 것만
+                            .sorted() // 정렬
+                            .toArray();
         
-        // 정렬
-        for(int i=0; i<arr.length; i++) {
-            for(int j=i+1; j<arr.length; j++) {
-                if(arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
+        // 나누어 떨어지지 않으면 -1
+        if(answer.length == 0) {
+            return new int[]{-1};
         }
-        
-        // 나누어 떨어지는 숫자 찾기
-        String numbers = "";
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] % divisor == 0) {
-                if("".equals(numbers)) {
-                    numbers += arr[i];
-                }else {
-                    numbers = numbers + "," + arr[i];
-                }
-            }
-        }
-        
-        // 나누어 떨어진 값이 없다면 -1
-        if(numbers.length() == 0) {
-            answer = new int[1];
-            answer[0] = -1;
-            return answer;
-        }
-        
-        String[] strArr = numbers.split(",");
-        answer = new int[strArr.length];
-        for(int i=0; i<strArr.length; i++) {
-            answer[i] = Integer.parseInt(strArr[i]);
-        }
-        
+
         return answer;
     }
 }
