5121f85b5163c36a35e1f5e871a4d03d0f232742
Seonoo <30854018+seonu5849@users.noreply.github.com>
2025-09-21 17:05:13 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 7.64 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    Set<Integer> set = new HashSet<>();
+    boolean[] visited;
+    
+    public int[] solution(int[] numbers) {
+        visited = new boolean[numbers.length];
+        dfs(numbers, 0, 0);
+        
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+    
+    void dfs(int[] numbers, int start, int depth) {
+        if(depth == 2){ // 선택한 숫자를 더하기
+            int sum = 0;
+            for(int i = 0; i < numbers.length; i++){
+                if(visited[i]){
+                    sum += numbers[i];
+                }
+            }
+            set.add(sum);
+            return;
+        }
+
+        // 2개의 숫자 선택
+        for(int i = start; i < numbers.length; i++){
+            visited[i] = true;
+            dfs(numbers, i + 1, depth + 1);
+            visited[i] = false;
+        }
+    }
+}
