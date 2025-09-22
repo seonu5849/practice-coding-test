0550c31cf956e0272d53991609ecb957e6fcc9bb
Seonoo <30854018+seonu5849@users.noreply.github.com>
2025-09-22 21:12:30 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.11 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder answer = new StringBuilder();
+        
+        for(int i=1; i<food.length; i++) {
+            for(int j=0; j<food[i]/2; j++) {
+                answer.append(i);
+            }
+        }
+        
+        StringBuilder right = new StringBuilder(answer).reverse();
+        
+        return answer.append("0").append(right).toString();
+    }
+}
