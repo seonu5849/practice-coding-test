55067721d73fec57c08d8b25449aa6131bb0087b
Seonoo <30854018+seonu5849@users.noreply.github.com>
2025-09-28 15:57:24 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 80.6 MB -BaekjoonHub

--- Java/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ Java/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,15 +1,11 @@
 class Solution {
     public String solution(String phone_number) {
-        StringBuilder sb = new StringBuilder();
+        char[] c = phone_number.toCharArray();
         
-        int len = phone_number.length();
-        String security = phone_number.substring(0, len - 4)
-                                      .replaceAll("[0-9]", "*");
+        for(int i=0; i<c.length-4; i++) {
+            c[i] = '*';
+        }
         
-        // 문자열 합치기
-        sb.append(security);
-        sb.append(phone_number.substring(len-4));
-        
-        return sb.toString();
+        return String.valueOf(c);
     }
 }
